2a0d39ab938fc4b52d6e8c1bb1271c5131d0c563
Seunghak Roh <tokirin1117@gmail.com>
2013-12-15 07:14:01 +0900

    minor bug fix

--- src/com/tokirin/whereapp/lib/Global.java
+++ src/com/tokirin/whereapp/lib/Global.java
@@ -2,7 +2,7 @@ package com.tokirin.whereapp.lib;
 
 public class Global {
 	
-	public static String host = "http://192.168.100.41";
+	public static String host = "http://j4.link-to-rink.com";
 	public static String port = "8920";
 
 }
